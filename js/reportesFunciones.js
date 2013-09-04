e019ed4ffdc07c02655fb841cfcfefe796a70d5d
admin <admin@user>
2013-09-04 13:24:34 -0500

    reporte cliente 50%

--- js/reportesFunciones.js
+++ js/reportesFunciones.js
@@ -23,6 +23,30 @@ function muestraFiltro(select){
         });
     }
     
+    if($(select).val() === '2'){
+        $("#menuIzq").html("");
+        $("#resultadoReporte").html("");
+        var url = "../reporte/clientes.php";
+        $.post(url,{},function (responseText){
+           $("#menuIzq").html(responseText); 
+           $("#btnRep").button();
+           $("#btnRep").click(function(){
+               if($("#lint_idPry").val() !== null){
+                    $("#mensaje").html("").removeClass("ui-state-error ui-corner-all");
+                    var url1 = "../reporte/reporteClientes.php";
+                    $.post(url1,{idPoE:$("#lint_idPoE").val()},function(data){
+                       $("#resultadoReporte").html(data);                      
+                       $("table tfoot td").nextAll("td").css({"border":"solid 1px","cellpading":"0px"});
+                    });
+               }else{
+                   $("#mensaje").html("<span>Seleccione por lo menos un cliente</span>").
+                           css({"color":"#cd0a0a","width": "231px","padding": "12px","margin-top": "31px"}).
+                           addClass("ui-state-error ui-corner-all");
+               }
+           });
+        });
+    }
+    
     if($(select).val() === '3'){
         $("#menuIzq").html("");
         $("#resultadoReporte").html("");
